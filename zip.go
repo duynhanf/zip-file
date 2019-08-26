f046f9866d360955c43ec236bd4a7998aeea986e
Bui Duy Nhan <fpt.mnes.nhanbd1@gmail.com>
2019-08-26 16:33:12 +0900

    add zip.go

---
+++ zip.go
@@ -0,0 +1,58 @@
+package zipFile
+
+import (
+	"time"
+	// "compress/flate"
+	// "io"
+	// "io/ioutil"
+	// "bytes"
+	"archive/zip"
+	"os"
+)
+
+type FileContent struct {
+	Name string
+	Content string
+}
+
+func ZipFolder(files []FileContent, dest string) error {
+
+	zf, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer zf.Close()
+
+	archive := zip.NewWriter(zf)
+	defer archive.Close()
+
+	// archive.RegisterCompressor(zip.Deflate, func(out io.Writer) (io.WriteCloser, error) {
+	// 	return flate.NewWriter(out, flate.BestCompression)
+	// })
+
+	for _, file := range files {
+		header := zip.FileHeader{
+			Name : file.Name,
+			Modified : time.Now(),
+		}
+	
+		z, err := archive.CreateHeader(&header)
+		if err != nil {
+			return err
+		}
+		_, err = z.Write([]byte(file.Content))
+		if err != nil {
+			return err
+		}
+	
+		
+	}
+
+	err = archive.Close()
+	if err != nil {
+		return err
+	}
+	
+
+	return nil
+}
